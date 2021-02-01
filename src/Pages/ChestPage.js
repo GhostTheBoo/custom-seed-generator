aa33ce53c1474fee92fb7cdabdc2c8733ca11283
GhostTheBoo <ghostthebooyt@gmail.com>
2021-02-01 10:19:30 -0800

    Added Help popup component and reorganized Chest Page

--- src/Pages/ChestPage.js
+++ src/Pages/ChestPage.js
@@ -9,22 +9,38 @@ import GenericSelect from '../Components/GenericSelect'
 import RewardSelect from '../Components/RewardSelect'
 import RewardTypeSelect from '../Components/RewardTypeSelect'
 import ChestTable from '../Tables/ChestTable'
+import HelpModal from '../Components/HelpModal'
 
 function ChestPage(props) {
+	const description = (
+		<div id="chestPageHelp">
+			<h6>Chest</h6>
+			<p>
+				Chests are seperated by the in game world they appear in.
+				After selecting a specific world, select what reward you wish to replace with.
+				When clicking replace, all checked rows will be replaced with whatever reward is in the reward selector.
+		</p>
+			<h6>Why Am I red?</h6>
+			<p>
+				Chests can contain anything so they will never show up as red.
+		</p>
+		</div>
+	)
+
 	return (
 		<div style={props.style}>
 			<Form>
 				<Form.Row>
-					<GenericSelect
-						class={'chest'}
-						selector={'World'}
-						itemList={worldsData}
-						name={'currentWorld'}
-						currentItem={props.chestData.currentWorld}
-						onChange={props.handleWorldChange}
-					/>
-				</Form.Row>
-				<Form.Row>
+					<Col>
+						<GenericSelect
+							class={'chest'}
+							selector={'World'}
+							itemList={worldsData}
+							name={'currentWorld'}
+							currentItem={props.chestData.currentWorld}
+							onChange={props.handleWorldChange}
+						/>
+					</Col>
 					<Col>
 						<RewardTypeSelect
 							class={'chest'}
@@ -57,14 +73,19 @@ function ChestPage(props) {
 				onClick={props.onClick}
 			>
 				REPLACE
-			</Button>
+				</Button>
 			{' '}
 			<Button variant='outline-dark'
 				name='vanillaButton'
 				onClick={props.onClick}
 			>
 				VANILLA
-			</Button>
+				</Button>
+			{' '}
+			<HelpModal
+				page={'Chest'}
+				description={description}
+			/>
 		</div>
 	)
 }
