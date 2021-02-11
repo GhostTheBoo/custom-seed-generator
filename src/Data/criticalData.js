80775fe0d7413d79a7f21f0c92add9b22f427ca9
GhostTheBoo <ghostthebooyt@gmail.com>
2021-02-10 20:11:33 -0800

    refactored criticalsData

--- src/Data/criticalData.js
+++ src/Data/criticalData.js
@@ -1,95 +1,21 @@
-const criticalData = [
-	{
-		vanillaReward: {
-			reward: 'Reaction Boost',
-			index: '0188'
-		},
-		replacementReward: {
-			reward: 'Reaction Boost',
-			index: '0188'
-		},
-		vanillaAddress: "11D18DDE",
-		toBeReplaced: false,
-		isReplaced: false
-	},
-	{
-		vanillaReward: {
-			reward: 'Finishing Plus',
-			index: '0189'
-		},
-		replacementReward: {
-			reward: 'Finishing Plus',
-			index: '0189'
-		},
-		vanillaAddress: "11D18DDC",
-		toBeReplaced: false,
-		isReplaced: false
-	},
-	{
-		vanillaReward: {
-			reward: 'Draw',
-			index: '0195'
-		},
-		replacementReward: {
-			reward: 'Draw',
-			index: '0195'
-		},
-		vanillaAddress: "11D18DE8",
-		toBeReplaced: false,
-		isReplaced: false
-	},
-	{
-		vanillaReward: {
-			reward: 'Lucky Lucky (1)',
-			index: '0197'
-		},
-		replacementReward: {
-			reward: 'Lucky Lucky (1)',
-			index: '0197'
-		},
-		vanillaAddress: "11D18DE4",
-		toBeReplaced: false,
-		isReplaced: false
-	},
-	{
-		vanillaReward: {
-			reward: 'Lucky Lucky (2)',
-			index: '0197'
-		},
-		replacementReward: {
-			reward: 'Lucky Lucky (2)',
-			index: '0197'
-		},
-		vanillaAddress: "11D18DE6",
-		toBeReplaced: false,
-		isReplaced: false
-	},
-	{
-		vanillaReward: {
-			reward: 'MP Hastera',
-			index: '01A5'
-		},
-		replacementReward: {
-			reward: 'MP Hastera',
-			index: '01A5'
-		},
-		vanillaAddress: "11D18DE0",
-		toBeReplaced: false,
-		isReplaced: false
-	},
-	{
-		vanillaReward: {
-			reward: 'No Experience',
-			index: '0194'
-		},
-		replacementReward: {
-			reward: 'No Experience',
-			index: '0194'
-		},
-		vanillaAddress: "11D18DE2",
-		toBeReplaced: false,
-		isReplaced: false
-	}
-]
+import { Reward } from './rewardsData'
 
-export default criticalData
+export class Critical {
+	constructor(vanilla, address) {
+		this.vanillaReward = vanilla
+		this.replacementReward = vanilla
+		this.vanillaAddress = address
+		this.toBeReplaced = false
+		this.isReplaced = false
+	}
+}
+
+export const criticalData = [
+	new Critical(new Reward('Reaction Boost', 0x0188), 0x11D18DDE),
+	new Critical(new Reward('Finishing Plus', 0x0189), 0x11D18DDC),
+	new Critical(new Reward('Draw', 0x0195), 0x11D18DE8),
+	new Critical(new Reward('Lucky Lucky (1)', 0x0197), 0x11D18DE4),
+	new Critical(new Reward('Lucky Lucky (2)', 0x0197), 0x11D18DE6),
+	new Critical(new Reward('MP Hastera', 0x01A5), 0x11D18DE0),
+	new Critical(new Reward('No Experience', 0x0194), 0x11D18DE2)
+]
