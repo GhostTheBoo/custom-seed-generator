7f81952be007788ee09ef338cc07b454bf3c1688
GhostTheBoo <ghostthebooyt@gmail.com>
2023-08-05 21:26:25 -0500

    Improve cost page functionality and styles

--- src/Components/GenericStatValue.js
+++ src/Components/GenericStatValue.js
@@ -3,17 +3,20 @@ import React from 'react'
 function GenericStatValue(props) {
     let textColor = '#FFFFFF'
 
-    if (props.statName.toLowerCase() === 'strength')
+    if (props.statName.toLowerCase().includes('str'))
         textColor = '#FF8080'
-    else if (props.statName.toLowerCase() === 'magic')
+    else if (props.statName.toLowerCase().includes('mag'))
         textColor = '#C080FF'
-    else if (props.statName.toLowerCase() === 'defense')
+    else if (props.statName.toLowerCase().includes('def'))
         textColor = '#FFF34B'
-    else if (props.statName.toLowerCase() === 'ap')
+    else if (props.statName.toLowerCase().includes('ap'))
         textColor = '#63C6F5'
 
     return (
-        <span style={{ fontFamily: 'KHGummi', color: textColor }}>
+        <span
+            className={props.className !== undefined ? props.className : ''}
+            style={{ fontFamily: 'KHGummi', color: textColor }}
+        >
             {/* {`${props.stat} ${props.children}`} */}
             {props.stat}
             {props.children}
--- src/cost/CostPage.js
+++ src/cost/CostPage.js
@@ -1,16 +1,15 @@
 import React, { useState, useEffect, useRef } from 'react'
+import { AnimatePresence, motion } from 'framer-motion'
 import './CostStyles.css'
 
 import GenericSelect from '../Components/GenericSelect'
-import NavbarIcon from '../navbar/NavbarIcon'
 
 import CostCard from './CostCard'
-import CostForm from './CostForm'
 
 function CostPage(props) {
+	const [currentFocus, setCurrentFocus] = useState(-1)
 	const [currentAbilityType, setCurrentAbilityType] = useState(0)
-	const [currentCategory, setCurrentCategory] = useState(-1)
-	const [currentCostFieldData, setCurrentCostFieldData] = useState([0, 0, 0, 0, 0])
+
 	const costCardGrid = useRef(null)
 	useEffect(() => {
 		costCardGrid.current.scrollTo({ top: 0, behavior: 'smooth' })
@@ -18,56 +17,21 @@ function CostPage(props) {
 
 	let currentAbilityTypeCategories = props.costData[currentAbilityType]
 
-	function handleCostChange(newCategory) {
-		if (newCategory !== -1) {
-			let newCosts = [0, 0, 0, 0, 0]
-			currentAbilityTypeCategories.abilities[newCategory].specificAbilities.forEach((specificAbility, sepcificAbilityIndex) => {
-				newCosts[sepcificAbilityIndex] = specificAbility.replacementCost
-			})
-			setCurrentCostFieldData(newCosts)
-		}
-		else setCurrentCostFieldData([0, 0, 0, 0, 0])
-	}
-
-	function handleCurrentAbilityTypeChange(newAbilityType) {
-		handleCurrentCategoryChange(-1)
-		setCurrentAbilityType(newAbilityType)
-	}
-
-	function handleCurrentCategoryChange(newCategory) {
-		handleCostChange(newCategory)
-		setCurrentCategory(newCategory)
-	}
-	function handleVanilla(vanillaCategoryTypeIndex) {
-		let newCategoryAbilities = currentAbilityTypeCategories.abilities.map((categoryType, categoryTypeIndex) => {
-			let newSpecificAbilities = categoryType.specificAbilities
-			if (categoryTypeIndex === vanillaCategoryTypeIndex) {
-				newSpecificAbilities = categoryType.specificAbilities.map(specificAbility => {
-					return specificAbility.vanilla()
+	function updateCost(newCategoryIndex, costAddress, newCost) {
+		let newCategoryAbilities = props.costData[currentAbilityType].abilities.map((category, categoryIndex) => {
+			let newSpecificAbilities = category.specificAbilities
+			if (categoryIndex === newCategoryIndex) {
+				newSpecificAbilities = category.specificAbilities.map(specificAbility => {
+					if (specificAbility.costAddress === costAddress)
+						return specificAbility.replace(newCost)
+					return specificAbility
 				})
 			}
 			return {
-				...categoryType,
+				...category,
 				specificAbilities: newSpecificAbilities
 			}
 		})
-		handleCurrentCategoryChange(-1)
-		updateAllCosts(newCategoryAbilities)
-	}
-	function updateCost() {
-		let newCategoryAbilities = currentAbilityTypeCategories.abilities.map((categoryType, categoryTypeIndex) => {
-			let newSpecificAbilities = categoryType.specificAbilities
-			if (categoryTypeIndex === currentCategory) {
-				newSpecificAbilities = categoryType.specificAbilities.map((specificAbility, specificAbilityIndex) => {
-					return specificAbility.replace(currentCostFieldData[specificAbilityIndex])
-				})
-			}
-			return {
-				...categoryType,
-				specificAbilities: newSpecificAbilities
-			}
-		})
-		handleCurrentCategoryChange(-1)
 		updateAllCosts(newCategoryAbilities)
 	}
 	function updateAllCosts(newAbilityTypes) {
@@ -83,17 +47,32 @@ function CostPage(props) {
 	}
 
 	let costList = currentAbilityTypeCategories.abilities.map((category, categoryIndex) => {
-		return (
-			<CostCard
-				key={'category' + categoryIndex}
-				id={'category' + categoryIndex}
-				category={category}
-				categoryIndex={categoryIndex}
-				isEditing={categoryIndex === currentCategory}
-				setAbility={handleCurrentCategoryChange}
-				setVanilla={handleVanilla}
-			/>
-		)
+		let cardList = category.specificAbilities.map(specificAbility => {
+			return (
+				<CostCard
+					key={specificAbility.costAddress}
+					id={specificAbility.costAddress}
+					category={category}
+					categoryIndex={categoryIndex}
+					ability={specificAbility}
+					isEditing={currentFocus === specificAbility.costAddress}
+					updateFocus={setCurrentFocus}
+					updateCost={updateCost}
+				/>
+			)
+		})
+		let image = <img
+			className='costCategoryImage'
+			src={`./images/${category.path}.png`}
+			alt={category.category}
+			width='100%'
+			height='auto'
+		/>
+
+		return <div key={`categoryGroup${category.category}`} className='costGridGroup'>
+			{image}
+			{cardList}
+		</div>
 	})
 
 	return (
@@ -114,7 +93,7 @@ function CostPage(props) {
 						itemList={props.costData.map(abilityType => { return abilityType.type })}
 						name={'currentAbilityType'}
 						currentItem={currentAbilityType}
-						onChange={(e) => handleCurrentAbilityTypeChange(parseInt(e.target.value))}
+						onChange={(e) => setCurrentAbilityType(parseInt(e.target.value))}
 					/>
 				</div>
 				<div className='flex-grow-1' />
@@ -129,7 +108,7 @@ function CostPage(props) {
 					key={`cost${currentAbilityType}`}
 					className='costPageContent'
 				>
-				<div className='costCardGrid flex-grow-1' ref={costCardGrid}>{costList}</div>
+					<div className='costCardGrid flex-grow-1' ref={costCardGrid}>{costList}</div>
 				</motion.div>
 			</AnimatePresence>
 		</div>
